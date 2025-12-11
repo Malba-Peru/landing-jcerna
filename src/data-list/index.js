fbbee5144e3a88b543bac21fe7b4160b0839b874
Ignacio2906 <ignacio.malcer@gmail.com>
2025-12-10 20:59:26 -0500

    fix mayus bug

--- src/data-list/index.js
+++ src/data-list/index.js
@@ -1,4 +1,4 @@
 export { services } from "./services.js";
-export { proyectsImages } from "./proyectsImages.js";
+export { proyImages } from "./proyImages.js";
 export { proyectsItems } from "./proyects.js";
 export { workflow } from "./workflow.js";
--- src/data-list/proyImages.js
+++ src/data-list/proyImages.js
@@ -1,4 +1,4 @@
-export const proyectsImages = Array.from({ length: 30 }, (_, i) => ({
+export const proyImages = Array.from({ length: 30 }, (_, i) => ({
   src: `/images/proyects_${i + 1}.webp`,
   alt: `Imagen de Proyectos Terminados ${i + 1}`,
 }));
