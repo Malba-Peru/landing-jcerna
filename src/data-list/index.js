7786c5d88a4d0760b49e6d4d44093eca4b017ae3
Ignacio2906 <ignacio.malcer@gmail.com>
2025-12-10 20:51:19 -0500

    fix images

--- src/data-list/index.js
+++ src/data-list/index.js
@@ -1,4 +1,4 @@
 export { services } from "./services.js";
+export {proyectsImages} from "./proyectsImages.js";
 export { proyectsItems } from "./proyects.js";
-export { proyectsImages } from "./proyectsImages.js";
 export { workflow } from "./workflow.js";
